9cc303f16061a2e1c4f2b701e18868fcde04cacb
kokobd <zelin.feng@pm.me>
2026-01-01 09:03:07 -0500

    support macos command key

--- init.lua
+++ init.lua
@@ -17,8 +17,15 @@ vim.opt.timeoutlen = 3000 -- 3 seconds - adjust as needed
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
--- Use system clipboard
-vim.opt.clipboard:append({ "unnamed", "unnamedplus" })
+-- MacOS command key
+if vim.g.neovide then
+	vim.keymap.set("n", "<D-s>", ":w<CR>") -- Save
+	vim.keymap.set("v", "<D-c>", '"+y') -- Copy
+	vim.keymap.set("n", "<D-v>", '"+P') -- Paste normal mode
+	vim.keymap.set("v", "<D-v>", '"+P') -- Paste visual mode
+	vim.keymap.set("c", "<D-v>", "<C-R>+") -- Paste command mode
+	vim.keymap.set("i", "<D-v>", '<C-R>+') -- Paste insert mode
+end
 
 -- Display settings
 vim.opt.termguicolors = true
